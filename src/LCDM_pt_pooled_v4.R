27ee2facaedb17828793a1d4af074c5f1fafa34f
adalisan <sadali@gmail.com>
2014-06-03 20:47:08 -0400

    simplified the code to run all tests in for loop
    add .bak to .gitignore

--- src/LCDM_pt_pooled_v4.R
+++ src/LCDM_pt_pooled_v4.R
@@ -1,9 +1,6 @@
 #pooled LCDM statistics analysis
 LCDM <- function(fileNames,condTests){
   
-
-  
-
   b1=TRUE; b2=TRUE; b3=TRUE; b4=TRUE; b5=TRUE; b6=TRUE; b7=TRUE;
   condTests<-c(b1,b2,b3,b4,b5,b6,b7)
   
@@ -80,6 +77,9 @@ LCDM <- function(fileNames,condTests){
 #fnx=c("data/s12349_lant_cing_AntsyGrey.txt","data/s12360_lant_cing_AntsyGrey.txt","data/s12497_lant_cing_AntsyGrey.txt")
 #fnx=c("data/CDR00_master.txt","data/CDR05_master.txt")
 
+
+p.cutoff <-0.001
+
 b <- c("Mann Whitney U Test","Kolmogorov-Smirnov Test Two-sided","Kolmogorov-Smirnov Test Greater","Kolmogorov-Smirnov Test Less","Welch's t-test","Kruskal-Wallis Test","ANOVA F-test")
 condTests<-c()
 condTests[1]="Mann Whitney U Test"%in%b
@@ -92,39 +92,49 @@ condTests[7]="ANOVA F-test"%in%b
 b<-condTests
 
 
-lh.pooled.LCDM.files <- c("lh_defscz.txt","lh_nondefscz.txt","lh_healthy")
-rh.pooled.LCDM.files <- c("rh_defscz.txt","rh_nondefscz.txt","rh_healthy")
+lh.pooled.LCDM.files <- c("lh_defscz.txt","lh_nondefscz.txt","lh_healthy.txt")
+rh.pooled.LCDM.files <- c("rh_defscz.txt","rh_nondefscz.txt","rh_healthy.txt")
+
+
+lh.pooled.LCDM.files <- paste("./data/",lh.pooled.LCDM.files,sep="")
+rh.pooled.LCDM.files <- paste("./data/",rh.pooled.LCDM.files,sep="")
+
 
 contrast.groups<-array(c(1,2,3,2,3,1,3,1,2),dim=c(3,3))
 sides<- c('lh','rh')
+test.names <- c("MWU Test" ,  "KS Test Two-sided",  "KS Test Greater" ,
+                "KS Test Less", "Welch's t-test", "KW Test", "ANOVA F Test")
 
-for (it in 1:3) 
-lh.out<-LCDM(lh.pooled.LCDM.files[contrast.groups[it,]],condTests)
-rh.out<-LCDM(rh.pooled.LCDM.files[contrast.groups[it,]],condTests)
-
-
-### write results into file ###
-#for (test.res in out){
-#  if (test.res$p.value<1E-5)
-#}
-
-
-for (side in sides){
-  if(length(out)!=0){
+out <- list()
+for (it in 1:3) {
+  for (side in sides){
+    
     if (side=='lh'){
-      out <- lh.out
+      out<-LCDM(lh.pooled.LCDM.files[contrast.groups[it,]],condTests)
+      
     }
     else{
-      out <- rh.out
-    }
+      out<-LCDM(rh.pooled.LCDM.files[contrast.groups[it,]],condTests)
+      
+    }    
+        
+    ### write results into file ###
+    #for (test.res in out){
+    #  if (test.res$p.value<1E-5)
+    #}
+    
+    fname<- paste0("./results/pt_pooled/",side,"_deno.txt")
+    
+    if(length(out)!=0){      
+      for (i in 1:length(test.names)){
+        test = test.names[i]
+        p.val = out[[i]]$p.value
         #echo $results.txt
-        if(b[1]){cat("p-value for MWU Test: ", out[[1]]$p.value,"\n",file="./results/pt_pooled/lh_deno.txt")}
-      if(b[2]){cat("p-value for KS Test Two-sided: ", out[[2]]$p.value,"\n",file="./results/pt_pooled/lh_deno.txt",append=TRUE)}
-      if(b[3]){cat("p-value for KS Test Greater: ", out[[3]]$p.value,"\n",file="./results/pt_pooled/lh_deno.txt",append=TRUE)}
-      if(b[4]){cat("p-value for KS Test Less: ", out[[4]]$p.value,"\n",file="./results/pt_pooled/lh_deno.txt",append=TRUE)}
-      if(b[5]){cat("p-value for Welch's t-test: ", out[[5]]$p.value,"\n",file="./results/pt_pooled/lh_deno.txt",append=TRUE)}
-      if(b[6]){cat("p-value for KW Test: ", out[[6]]$p.value,"\n",file="./results/pt_pooled/lh_deno.txt",append=TRUE)}
-      if(b[7]){cat("p-value for ANOVA F Test: ", out[[7]]$p.value,"\n",file="./results/pt_pooled/lh_deno.txt",append=TRUE)}
+        if(b[i]){
+          cat("p-value for ",test, ": ",p.val,"\n",file=fname,append=(i>1))
+          if(!is.null(p.val) && p.val<p.cutoff) cat("**Significant \n",file=fname,append=TRUE)
+        }
+      }
       
       #Translating directly by unlisting
       #if(b[1]){cat("Result for MWU Test: ",unlist(lh.out[[1]]),"\n",file="result.txt")}
@@ -133,7 +143,8 @@ for (side in sides){
       #if(b[4]){cat("\nResult for KW Test: ",unlist(lh.out[[4]]),"\n",file="result.txt",append=TRUE)}
       #if(b[5]){cat("\nResult for ANOVA F Test: ",unlist(lh.out[[5]]),"\n",file="result.txt",append=TRUE)}
     } else { 
-      cat("No test was performed",file="result.txt",append=TRUE)
+      cat("No test was performed",file=fname,append=TRUE)
     }
-  
+    
+  }
 }
